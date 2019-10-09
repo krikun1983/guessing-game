90836d0e26b11bbc0cb291262bc8b878c8b61165
Stanislav <akhmadeevst@gmail.com>
2019-10-09 22:50:42 +0400

    feat: add guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+//floor
     guess() {
-
+        this.middle = Math.round((this.min + this.max)/2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
+        return this.max;
     }
 
     greater() {
-
+        this.min = this.middle;
+        return this.min;
     }
 }
 
